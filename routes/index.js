8706a470b3e6285a058fa1509278401122413ed6
Anush Mattapalli <matanatr96@gmail.com>
2017-01-26 21:33:01 -0500

    could not figure out pug...."

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Pretty cool'});
+    res.render('index', { title: 'Homepage'});
 });
 
 router.get('/test/:id', function (req, res, next) {
