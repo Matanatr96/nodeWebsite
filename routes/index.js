e1a346cebc60fae37450e00b96145c5c954b099a
Anush Mattapalli <matanatr96@gmail.com>
2017-01-30 15:35:59 -0500

    rearranged website sending about me to gt page

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Homepage'});
 });
 
 router.get('/aboutMe', function (req, res, next) {
     res.render('aboutMe', {title: 'About Me'})
+});
+
+router.get('/projects', function (req, res, next) {
+    res.render('projects', {title: 'projects'})
+});
+
+router.get('/gt', function (req, res, next) {
+    res.render('gt', {title: 'School'})
 });
 
 module.exports = router;
