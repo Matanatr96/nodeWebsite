0088772b9c2f7ca3aa215bc01f4dd52dfd22b933
Anush Mattapalli <matanatr96@gmail.com>
2017-04-03 21:53:24 -0400

    Couldnt figure out socket.io

--- app.js
+++ app.js
@@ -8,15 +8,13 @@ var express = require('express'),
 
     app = express(),
     index = require('./routes/index'),
+    fun = require('./routes/fun'),
     projects = require('./routes/projects');
 
 //routing to controllers
+app.use('/fun', fun);
 app.use('/projects', projects);
 app.use('/', index);
-
-
-
-
 
 
 // view engine setup
