996db9a78611965cf08e23df3f0a812e19ae677e
Anush Mattapalli <matanatr96@gmail.com>
2017-04-19 18:37:11 -0400

    added more words to about me.

--- public/js/intro.js
+++ public/js/intro.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    alert('Please note: This website is under development. What you see now may or may not be in the final version of this site.');
     setTimeout(function () {
         $('#img').fadeOut('slow', function () {
             $('#nonImage').fadeIn('slow');
